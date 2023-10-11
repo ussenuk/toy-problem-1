225887642f698473371d3127ad5b96acd5d74a07
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-11 21:04:38 +0300

    challenge2 updated and done

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # toy-problem-1
+## task
 write a program that takes the speed of a car as input e.g 80. If the speed is less than 70, it should print "OK". Otherwise, for every 5km/s above the speed limit(70), it should give the driver one demerit point and print the total number of demerit points.
 for example, if the speed is 80, it should print. "Point:2:".If the driver gets more then 12 points, the function should print: "License suspended".
+ 
+## Usage
+
+1. Open `index.html` in your web browser.
+2. Enter the speed of the car in the input field.
+3. Click the "Check Speed" button.
+4. The program will display whether it's "OK" or the number of demerit points.
--- index.js
+++ index.js
@@ -1,19 +1,77 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const speedOfCar = document.getElementById("speedOfCar");
+//   const checkSpeedBtn = document.getElementById("checkSpeedBtn");
+//   const result = document.getElementById("result");
+
+//   checkSpeedBtn.addEventListener("click", function () {
+//     const speed = parseInt(speedOfCar.value);
+//     checkSpeed(speed);
+//   });
+// });
+
+// // function checkSpeed(speed) {
+// //   const speedLimit = 70;
+// //   const kmPerPoint = 5;
+
+// //   if (speed <= speedLimit) {
+// //     result.textContent = "OK";
+// //   } else {
+// //     const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+// //     if (points >= 12) {
+// //       result.textContent = `Points: ${points}. License suspended.`;
+// //     } else {
+// //       result.textContent = `Points: ${points}`;
+// //     }
+// //   }
+// // }
+
+//   function checkSpeed(speed) {
+//     const speedLimit = 70;
+//     const demeritPointsPerKmOverLimit = 5;
+//     const maxDemeritPoints = 12;
+
+//     if (speed <= speedLimit) {
+//     //   console.log("OK");
+//     result.textContent = "OK";
+//     } else {
+//       const demeritPoints = Math.floor(
+//         (speed - speedLimit) / demeritPointsPerKmOverLimit
+//       );
+
+//       if (demeritPoints >= maxDemeritPoints) {
+//         // console.log("License suspended");
+//         result.textContent = "License suspended";
+//       } else {
+//         // console.log(`Points: ${demeritPoints}`);
+//         result.textContent = `Points: ${demeritPoints}`;
+//       }
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const speedOfCar = document.getElementById("speedOfCar");
+  const checkSpeedBtn = document.getElementById("checkSpeedBtn");
+  const result = document.getElementById("result");
+
+  checkSpeedBtn.addEventListener('click', function () {
+    const speed = parseInt(speedOfCar.value);
+    checkSpeed(speed);
+  });
+
   function checkSpeed(speed) {
     const speedLimit = 70;
-    const demeritPointsPerKmOverLimit = 5;
-    const maxDemeritPoints = 12;
-
+    const speedCar = 5;
     if (speed <= speedLimit) {
-      console.log("OK");
+      result.textContent = "OK";
     } else {
-      const demeritPoints = Math.floor(
-        (speed - speedLimit) / demeritPointsPerKmOverLimit
-      );
+      const points = Math.floor((Math.abs(speed) - speedLimit) / speedCar);
 
-      if (demeritPoints >= maxDemeritPoints) {
-        console.log("License suspended");
+      if (points >= 12) {
+        result.textContent = `Points: ${points}. License suspended.`;
       } else {
-        console.log(`Points: ${demeritPoints}`);
+        result.textContent = `Points: ${points}`;
       }
     }
-}
+  }
+});
